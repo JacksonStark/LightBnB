a973a36a5aa77116821a3a985c0c96eed4726cb5
Jackson Stark <jacksonstark77@hotmail.com>
2019-10-17 15:23:15 +0000

    update functions to use the SQL database

---
+++ LightBnB_WebApp-master/server/database.js
@@ -0,0 +1,115 @@
+const properties = require('./json/properties.json');
+const users = require('./json/users.json');
+const { Pool } = require('pg');
+
+const pool = new Pool ({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+/// USERS
+
+/**
+ * Get a single user from the database given their email.
+ * @param {String} email The email of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getUserWithEmail = function(email) {
+
+  return pool.query(`
+    SELECT *
+    FROM users
+    WHERE email = $1
+  `, [email])
+  .then(res => res.rows[0])
+  .catch(err => err);
+}
+exports.getUserWithEmail = getUserWithEmail;
+
+/**
+ * Get a single user from the database given their id.
+ * @param {string} id The id of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getUserWithId = function(id) {
+  return pool.query(`
+    SELECT *
+    FROM users
+    WHERE id = $1
+  `, [id])
+  .then(res => res.rows[0])
+  .catch(err => err);
+
+  // return Promise.resolve(users[id]);
+}
+exports.getUserWithId = getUserWithId;
+
+
+/**
+ * Add a new user to the database.
+ * @param {{name: string, password: string, email: string}} user
+ * @return {Promise<{}>} A promise to the user.
+ */
+const addUser =  function(user) {
+  return pool.query(`
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *
+  `, [user.name, user.email, user.password])
+  .then(res => res.rows[0])
+  .catch(err => err);
+}
+exports.addUser = addUser;
+
+/// Reservations
+
+/**
+ * Get all reservations for a single user.
+ * @param {string} guest_id The id of the user.
+ * @return {Promise<[{}]>} A promise to the reservations.
+ */
+const getAllReservations = function(guest_id, limit = 10) {
+  return getAllProperties(null, 2);
+}
+exports.getAllReservations = getAllReservations;
+
+/// Properties
+
+/**
+ * Get all properties.
+ * @param {{}} options An object containing query options.
+ * @param {*} limit The number of results to return.
+ * @return {Promise<[{}]>}  A promise to the properties.
+ */
+const getAllProperties = function(options, limit = 10) {
+
+  return pool.query(`
+    SELECT * FROM properties
+    LIMIT $1
+  `, [limit])
+  .then(res => res.rows)
+  .catch(err => err);
+
+  // const limitedProperties = {};
+  // for (let i = 1; i <= limit; i++) {
+  //   limitedProperties[i] = properties[i];
+  // }
+  // return Promise.resolve(limitedProperties);
+}
+exports.getAllProperties = getAllProperties;
+
+
+/**
+ * Add a property to the database
+ * @param {{}} property An object containing all of the property details.
+ * @return {Promise<{}>} A promise to the property.
+ */
+const addProperty = function(property) {
+  const propertyId = Object.keys(properties).length + 1;
+  property.id = propertyId;
+  properties[propertyId] = property;
+  return Promise.resolve(property);
+}
+exports.addProperty = addProperty;
---
+++ LightBnB_WebApp-master/server/server.js
@@ -0,0 +1,38 @@
+const database = require('./database');
+const apiRoutes = require('./apiRoutes');       // APIROUTES.js
+const userRoutes = require('./userRoutes');     // USERROUTES.js
+
+const path = require('path');
+
+const express = require('express');
+const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1']
+}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// /api/endpoints
+const apiRouter = express.Router();
+apiRoutes(apiRouter, database);                 // APIROUTES.js
+app.use('/api', apiRouter);
+
+// /user/endpoints
+const userRouter = express.Router();
+userRoutes(userRouter, database);               // USERROUTES.js
+app.use('/users', userRouter);
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get("/test", (req, res) => {
+  res.send("🤗");
+});
+
+const port = process.env.PORT || 3000; 
+app.listen(port, (err) => console.log(err || `listening on port ${port} 😎`));
